7848f558c59a800da76e2424ecb6318e3fdd1bab
Your Name <your.email@example.com>
2025-06-11 11:44:12 +0530

    To-Do Application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>ToDo Application</h1>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
